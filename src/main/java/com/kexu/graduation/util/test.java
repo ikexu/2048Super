c6049868fad189707afbed3f87b9d0ac165dfd70
meitianxiaoxu <1103028349@qq.com>
2017-04-10 22:20:04 +0800

    test

---
+++ src/main/java/com/kexu/graduation/util/test.java
@@ -0,0 +1,7 @@
+package com.kexu.graduation.util;
+
+/**
+ * Created by KeXu on 2017/4/10.
+ */
+public class test {
+}
---
+++ src/main/scala/com/kexu/graduation/algorithm/test.scala
@@ -0,0 +1,8 @@
+package com.kexu.graduation.algorithm
+
+/**
+  * Created by KeXu on 2017/4/10.
+  */
+class test {
+
+}
