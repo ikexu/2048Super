858e269738f2d4696410053a70f94fcbf23853ef
meitianxiaoxu <1103028349@qq.com>
2017-04-16 15:59:31 +0800

    add GridDirect

--- 2048-core/src/main/scala/graduation/util/Grid.scala
+++ 2048-core/src/main/scala/graduation/util/Grid.scala
@@ -5,10 +5,7 @@ package graduation.util
   */
 class Grid(uuid:String) extends Serializable{
 
-  val id:String=uuid
   val data:Array[Array[Int]]=Array.ofDim[Int](2,4)
-
-
 
   def getRow(rowIndex:Int):(Int,Int,Int,Int)={
 
@@ -19,5 +16,15 @@ class Grid(uuid:String) extends Serializable{
   def getCol(colIndex:Int):(Int,Int,Int,Int)={
     (data(0)(colIndex),data(1)(colIndex),data(2)(colIndex),data(3)(colIndex))
   }
+
+}
+
+object GridDirect extends Enumeration{
+
+  type GridDirect=Value
+  val Up=Value(0)
+  val Down=Value(1)
+  val Left=Value(2)
+  val Right=Value(3)
 
 }
