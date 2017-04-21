f7a25b3116ed7bbcd3f160418d05ce73320cf915
WendellSun <iwendellsun@gmail.com>
2017-04-21 23:48:25 +0800

    Add ai logic && add Dockerfile

---
+++ 2048-core/src/main/scala/graduation/algorithm/AI.scala
@@ -0,0 +1,77 @@
+package graduation.algorithm
+
+import graduation.models.Grid
+
+import scala.collection.mutable.ListBuffer
+
+/**
+  * Created by wendell on 21/4/2017.
+  */
+class AI (var d:Grid){
+  var grid=d;
+
+   def search(dept:Int,alpha:Double,beta: Double): (Grid.Direct,Double) ={
+     var bestScore: Double=0
+     var bestMove: Grid.Direct=Grid.NONE
+     var result:(Grid.Direct,Double)=()
+
+     if(grid.playerTurn){
+       // 该玩家选择方向
+       bestScore=alpha
+       for(direct <- List(Grid.UP,Grid.LEFT,Grid.DOWN,Grid.RIGHT)){
+         val newGrid=grid.clone()
+         if(newGrid.move(direct)){
+           val newAI = AI(newGrid)
+           // 深度为0，返回此时最好的中间局
+           if(dept==0){
+             result = (bestMove,grid.eval())
+           }else{
+             result=search(dept-1,bestScore,beta)
+           }
+           if(result._2>bestScore){
+             bestScore=result._2
+             bestMove=direct
+           }
+           if(bestScore>beta){
+             return (bestMove,beta)
+           }
+         }
+       }
+     }else{
+       // 该电脑选择放入数字
+       bestScore=beta
+       var cells=grid.availableCells()
+       var badScore=100.0;
+       var badCells=ListBuffer[((Int,Int),Int)]()
+       List(2,4).foreach(value=>{
+         cells.foreach(cell=>{
+           grid.setCell(cell,value)
+           val score=grid.smoothness()+grid.islands()
+           if(score<badScore){
+             badScore=score
+             badCells=ListBuffer((cell,value))
+           }else if(score==badScore){
+             badCells+=((cell,value))
+           }
+           grid.setCell(cell,0)
+         })
+       })
+       badCells.foreach(badValue=>{
+         result=AI(grid.clone().setCell(badValue._1,badValue._2)).search(dept,alpha,bestScore)
+         if(result._2<bestScore){
+           bestScore=result._2
+         }
+         if(bestScore<alpha){
+           return (Grid.NONE,alpha)
+         }
+       })
+     }
+     return (bestMove,bestScore)
+   }
+
+}
+
+object AI{
+  def apply(g:Grid)=new AI(g)
+}
+
--- 2048-core/src/main/scala/graduation/models/Grid.scala
+++ 2048-core/src/main/scala/graduation/models/Grid.scala
@@ -30,7 +30,35 @@ class Grid(val k:String, val p:Boolean,val s:Int,var d:Array[Array[Int]]) extend
     */
   def playerCutover():Boolean={
     playerTurn = !playerTurn
+    playerTurn
   }
+
+  // 朝某个方向移动后的局面,如果不能移动返回false
+  def move(direct:Grid.Direct):Boolean = ???
+
+  // 克隆该Grid对象
+  override def clone(): Grid = ???
+
+  // 评价该局面的评分
+  def eval(): Double = ???
+
+  // 返回空格的索引数组
+  def availableCells():Array[(Int,Int)] = ???
+
+  // 添加格子，设置value为0即为删除格子
+  def setCell(index:(Int,Int) , value: Int):Grid = ???
+
+  // 计算局面的平滑性
+  def smoothness():Double = ???
+
+  // 计算局面孤立点的数目
+  def islands() = ???
+
+  // 计算局面的单调性
+  def monotonicity():Double= ???
+
+  // 局面中最大的值
+  def maxValue():Int= ???
 
 }
 
@@ -42,6 +70,8 @@ object Grid extends Enumeration{
   val LEFT = Value("left")
   val DOWN = Value("down")
   val RIGHT = Value("right")
+  val NONE = Value("none")
+  val directs=List(UP,LEFT,DOWN,RIGHT)
 
   def apply(k: String,p: Boolean,s: Int,d: Array[Array[Int]]) = new Grid(k,p,s,d)
 
