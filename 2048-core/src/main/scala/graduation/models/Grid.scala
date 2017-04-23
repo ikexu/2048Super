f44355573798adade08cee438a16085e76a832d3
meitianxiaoxu <1103028349@qq.com>
2017-04-23 19:15:36 +0800

    add move Grid function.fix clone array bug

--- 2048-core/src/main/scala/graduation/models/Grid.scala
+++ 2048-core/src/main/scala/graduation/models/Grid.scala
@@ -5,7 +5,7 @@ import Argonaut._
 import org.apache.avro.SchemaBuilder.ArrayBuilder
 
 import scala.collection.mutable.ArrayBuffer
-
+import util.control.Breaks._
 
 /**
   * Created by KeXu on 2017/4/13.
@@ -39,22 +39,117 @@ class Grid(val k:String, val p:Boolean,val s:Int,var d:Array[Array[Int]]) extend
   // 朝某个方向移动后的局面,如果不能移动返回false
   def move(direct:Grid.Direct):Boolean = {
       direct match {
-        case Grid.UP => println("上")
-
-
-        case Grid.DOWN => println("下")
-
-        case Grid.LEFT => println("左右")
-
-        case Grid.RIGHT => println("右")
+        //move up
+        case Grid.UP =>
+          val baseArray=data.map(_.clone()).clone()
+        for(y <- 0 to 3){
+          var x=0
+          while (x<4){
+            breakable{
+              for(x1 <- (x+1) to 3){
+                if(data(x1)(y) > 0){
+                  if (data(x)(y) <= 0) {
+                    setCell((x,y),data(x1)(y))
+                    setCell((x1,y),0)
+                    x=x-1
+                  } else if (data(x)(y) == (data(x1)(y))) {
+                    setCell((x,y),data(x1)(y) * 2)
+                    setCell((x1,y),0)
+                  }
+                  break()
+                }
+              }
+            }
+            x=x+1
+          }
+        }
+          !equalData(baseArray,data)
+        //move down
+        case Grid.DOWN =>
+          val baseArray=data.map(_.clone()).clone()
+          for(y <- 0 to 3){
+            var x=3
+            while (x>=0){
+              breakable {
+                var x1=x-1
+              while(x1>=0){
+                if (data(x1)(y) > 0) {
+                  if (data(x)(y) <= 0) {
+                    setCell((x,y),data(x1)(y))
+                    setCell((x1,y),0)
+                    x=x+1
+                  } else if (data(x)(y) == data(x1)(y)) {
+                    setCell((x,y),data(x1)(y) * 2)
+                    setCell((x1,y),0)
+                  }
+                  break()
+                }
+                x1=x1-1
+              }
+              }
+              x=x-1
+            }
+          }
+         !equalData(baseArray,data)
+        //move left
+        case Grid.LEFT =>
+          val baseArray=data.map(_.clone()).clone()
+          for(x <- 0 to 3){
+            var y=0
+            while (y<4){
+              breakable {
+                for(y1 <- (y+1) to 3){
+                  if(data(x)(y1) > 0){
+                    if (data(x)(y) <= 0) {
+                      setCell((x,y),data(x)(y1))
+                      setCell((x,y1),0)
+                      y=y-1
+                    } else if (data(x)(y) == (data(x)(y1))) {
+                      setCell((x,y),data(x)(y1) * 2)
+                      setCell((x,y1),0)
+                    }
+                    break()
+                  }
+                }
+              }
+            y=y+1
+            }
+          }
+          !equalData(baseArray,data)
+          //move right
+        case Grid.RIGHT =>
+          val baseArray=data.map(_.clone()).clone()
+          for(x <- 0 to 3){
+            var y=3
+            while (y>=0){
+              breakable {
+                var y1=y-1
+                while(y1>=0){
+                  if (data(x)(y1) > 0) {
+                    if (data(x)(y) <= 0) {
+                      setCell((x,y),data(x)(y1))
+                      setCell((x,y1),0)
+                      y=y+1
+                    } else if (data(x)(y) == data(x)(y1)) {
+                      setCell((x,y),data(x)(y1) * 2)
+                      setCell((x,y1),0)
+                    }
+                    break;
+                  }
+                  y1=y1-1
+                }
+              }
+              y=y-1
+            }
+          }
+          !equalData(baseArray,data)
       }
-    true
 
   }
 
   // 克隆该Grid对象
   override def clone(): Grid = {
-    val copydata=data.clone()
+    val copydata=data.map(_.clone()).clone()
     new Grid(key,playerTurn,step,copydata)
   }
 
@@ -78,6 +173,16 @@ class Grid(val k:String, val p:Boolean,val s:Int,var d:Array[Array[Int]]) extend
   def setCell(index:(Int,Int) , value: Int):Grid = {
     data(index._1)(index._2)=value
     this
+  }
+
+  def equalData(data1:Array[Array[Int]],data2:Array[Array[Int]]):Boolean={
+    for(row <- 0 to 3;col <- 0 to 3)
+      {
+          if(data1(row)(col) != data2(row)(col)){
+           return false
+          }
+      }
+    true
   }
 
   // 计算局面的平滑性
--- 2048-core/src/main/scala/graduation/util/JsonUtil.scala
+++ 2048-core/src/main/scala/graduation/util/JsonUtil.scala
@@ -16,10 +16,10 @@ object JsonUtil {
       """
         |{"data":
         |[
-        |[1,2,3,4],
-        |[0,0,0,0],
-        |[0,0,64,0],
-        |[0,0,0,0]
+        |[2,2,4,4],
+        |[0,0,0,4],
+        |[8,2,64,8],
+        |[4,0,4,0]
         |],
         |"step":10,
         |"playerTurn":true,
@@ -36,7 +36,9 @@ object JsonUtil {
     /*println(grid.data+" "+grid)
     val newGrid=grid.clone()
     println(newGrid.data+" "+newGrid)*/
-    grid.move(Grid.LEFT)
+
+    println(grid.move(Grid.RIGHT))
+    grid.data.foreach(i=>println(i.mkString(" ")))
 
 
 
